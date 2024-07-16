d602f0eee3fcc301f5fecc133d00c03589d20b73
Arun02DS <arun02negime@gmail.com>
2024-07-16 22:37:48 +0530

    Q and A bot

--- QqndAbot.py
+++ QqndAbot.py
@@ -15,14 +15,34 @@ chat=model.start_chat(history=[])
 
 def get_responce(prompt):
     response = chat.send_message(prompt,stream=True)
-    return response.text
+    return response
 
 st.set_page_config(page_title="Contact with us", page_icon="🎧", layout="centered",)
 
 st.header("Gemini LLM Application")
 
 #Initailize the process for chat history
-if ''
+if 'chat_history' not in st.session_state:
+    st.session_state["chat_history"]=[]
+
+input=st.text_input("Input: ",key="input" )
+
+submit = st.button("Ask?")
+
+if submit and input:
+    response = get_responce(input)
+    st.session_state["chat_history"].append(["you",input])
+    st.subheader("Qurey response:")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state["chat_history"].append(["Bot",chunk.text])
+
+st.subheader("chat history is")
+
+for role,text in st.session_state["chat_history"]:
+    st.write(f"{role}:{text}")
+
+    # st.write(response)
 
 
 
